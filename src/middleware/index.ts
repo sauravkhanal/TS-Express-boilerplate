49c402fb6af9381513bb191a28e5c02db2bcd50f
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-15 14:38:49 +0545

    add static folder

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -6,6 +6,7 @@ const middleware = express()
 middleware.use(express.json())
 middleware.use(express.urlencoded({extended: false}))
 middleware.use(cookieParser())
+middleware.use(express.static("public"))
 
 
 export default middleware
