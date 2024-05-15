260a740f35b7e8335862f43d12d9e71ee91b0c12
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-15 14:31:54 +0545

    add router for echoing
    cookies, form-encode, query params, url params

--- src/routes/base/base.controllers.ts
+++ src/routes/base/base.controllers.ts
@@ -1,34 +1,64 @@
 import { Request, Response } from "express";
 import ApiResponse from "../../utils/ApiResponse";
 
-
 function defaultResponse(req: Request, res: Response) {
-
 	return res.status(200).json(
-		new ApiResponse(200, "This is message", {
+		new ApiResponse(200, "This is message.", {
 			"key field": "value field",
 		})
 	);
 }
 
 function echoBody(req: Request, res: Response) {
-
 	return res.status(200).json(
-		new ApiResponse(200, "This is message", {
-            body: req.body
+		new ApiResponse(200, "Echo json body request received.", {
+			body: req.body,
 		})
 	);
 }
 
 function echoCookie(req: Request, res: Response) {
-
-    return res.status(200).json(
-        new ApiResponse(200, "Cookie request received", {
-            cookie: req?.cookies
-        })
-    )
+	return res.status(200).json(
+		new ApiResponse(200, "Cookie request received.", {
+			cookie: req?.cookies,
+		})
+	);
 }
 
+function echoFormEncode(req: Request, res: Response) {
+	return res.status(200).json(
+		new ApiResponse(200, "Form data echo request received.", {
+			formData: req.body,
+		})
+	);
+}
 
-const baseController = {defaultResponse, echoBody, echoCookie}
-export default baseController
+function echoQueryParams(req: Request, res: Response) {
+	return res
+		.status(200)
+		.json(
+			new ApiResponse(200, "Echo query params request received.", {
+				queryParams: req.query,
+			})
+		);
+}
+
+function echoUrlParams(req: Request, res: Response) {
+	return res
+		.status(200)
+		.json(
+			new ApiResponse(200, "Echo params request received.", {
+				queryParams: req.params,
+			})
+		);
+}
+
+const baseController = {
+	defaultResponse,
+	echoBody,
+	echoCookie,
+	echoFormEncode,
+	echoQueryParams,
+	echoUrlParams
+};
+export default baseController;
--- src/routes/base/index.ts
+++ src/routes/base/index.ts
@@ -7,7 +7,10 @@ const baseRoute = Router();
 
 baseRoute.get("/cookie", baseController.echoCookie)
 baseRoute.get("/", baseController.defaultResponse)
-baseRoute.route("/").post(baseController.echoBody)
+baseRoute.route("/json").post(baseController.echoBody)
+baseRoute.route("/form-encode").post(baseController.echoFormEncode)
+baseRoute.route("/query").get(baseController.echoQueryParams)
+baseRoute.route("/param/:key_of_param").get(baseController.echoUrlParams)
 
 
 export default baseRoute
