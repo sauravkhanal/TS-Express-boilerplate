959c73942dd239d8b932486af5f9aad5a95562ab
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-21 17:01:14 +0545

    add mongodb connection

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import env from "./src/config/env";
 import router from "./src/routes";
 import middleware from "./src/middleware";
 import { errorHandler } from "./src/utils/ErrorHandler";
-
+import { connectDB } from "./src/utils/ConnectDB";
 
 // initialize server
 const app = express();
@@ -17,10 +17,17 @@ app.use(router);
 //use global error handler
 app.use(errorHandler);
 
-
-// run server and listen for incoming requests
-const listenCallback = () => {
-	const url = `http://localhost:${env.port}`;
-	console.log(`Server running at \x1b[35m${url}\x1b[0m.`);
+const startServer = async () => {
+	try {
+		await connectDB();
+		if (! env.port) throw new Error("PORT number not provided")
+		app.listen(env.port, () => {
+			const url = `http://localhost:${env.port}`;
+			console.log(`Server running at \x1b[35m${url}\x1b[0m.`);
+		});
+	} catch (error) {
+		console.log(`Failed to start server: \x1b[31m${error}\x1b[0m.`);
+	}
 };
-app.listen(env.port, listenCallback);
+
+startServer();
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-	port: process.env.PORT || 5000,
+	port: process.env.PORT,
 	cors: process.env.CORS || "*",
-	endpoint: process.env.ENDPOINT || `http://localhost:${process.env.PORT || 5000}`
+	endpoint: process.env.ENDPOINT || `http://localhost:${process.env.PORT || 5000}`,
+	mongoDBURI: process.env.MONGO_DB_URI,
+	DBName: process.env.DB_NAME || "BOILERPLATE-TEST",
 };
