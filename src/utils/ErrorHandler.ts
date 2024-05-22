8c2ece76b46a94e4057a937d36de8bcc28f1ef9e
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 00:01:55 +0545

    customize global error handler

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,23 +1,72 @@
 import { Request, Response, NextFunction } from "express";
 import multer from "multer";
 import { failureResponse } from "./ApiResponse";
+import mongoose from "mongoose";
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    console.error("An error occurred:", err);
+interface ErrorResponse {
+    statusCode: number;
+    message: string;
+    errors?: { [key: string]: string }, // array of key value pair of string type
+}
 
-    let statusCode = 500;
-    let message = "Internal Server Error !!!!"
 
-    // // Customize error handling based on the type of error
-    // if (err instanceof CustomError) {
-    //     // Custom error handling logic
-    //     statusCode = err.statusCode;
-    //     message = err.message;
-    // }
+export function errorHandler(error: unknown, req: Request, res: Response, next: NextFunction) {
+    let errorResponse: ErrorResponse = {
+        statusCode: 500,
+        message: "Internal Server Error",
+    };
 
-    if (err instanceof multer.MulterError) {
-        message = `Multer error: ${err.message}`;
+    if (error instanceof multer.MulterError) errorResponse = handleMulterError(error)
+    else if (typeof error === 'object' && error && 'code' in error && error.code === 11000) errorResponse = handleKeyDuplicationError(error);
+    else if (error instanceof mongoose.Error.CastError) errorResponse = handleCastError(error);
+    else if (error instanceof mongoose.Error.ValidationError) errorResponse = handleValidationError(error);
+
+    return failureResponse(res, errorResponse.statusCode, errorResponse.message, errorResponse.errors)
+}
+
+
+function handleMulterError(error: multer.MulterError): ErrorResponse {
+    return {
+        statusCode: 400,
+        message: `Multer error: ${error.message}`
     }
+}
 
-    return failureResponse(res, statusCode, message)
+
+function handleCastError(error: mongoose.CastError): ErrorResponse {
+    const errors: { [key: string]: string } = {};
+    errors[error.path] = 'Invalid value';
+    return {
+        statusCode: 400,
+        message: `Api Validation Error`,
+        errors
+    }
+}
+
+
+function handleKeyDuplicationError(error: any): ErrorResponse {
+    const errors: { [key: string]: string } = {};
+    for (const key in error.keyPattern) {
+        if (error.keyPattern.hasOwnProperty(key)) {
+            errors[key] = `The ${key.toLowerCase()} already exists`;
+        }
+    }
+    return {
+        statusCode: 409,
+        message: 'API Validation Error',
+        errors,
+    };
+}
+
+
+function handleValidationError(error: mongoose.Error.ValidationError): ErrorResponse {
+    const errors: { [key: string]: string } = {};
+    Object.values(error.errors).forEach(el => {
+        errors[el.path] = el.message;
+    });
+    return {
+        statusCode: 400,
+        message: 'API Validation Error',
+        errors,
+    };
 }
