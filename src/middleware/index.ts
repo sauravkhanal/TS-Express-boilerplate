8824d4f0e3fe355960ab0cd9abc0890738d213b0
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 11:26:40 +0545

    add middleware to decode access token, append in res.locals

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import decodeAccessToken from "./decodeAccessToken"
 
 const middleware = express()
 
@@ -7,6 +8,7 @@ middleware.use(express.json())
 middleware.use(express.urlencoded({extended: false}))
 middleware.use(cookieParser())
 middleware.use(express.static("public"))
+middleware.use(decodeAccessToken)
 
 
 export default middleware
