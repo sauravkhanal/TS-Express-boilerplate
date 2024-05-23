ae57357728e0efcfa8169d3b66f7e175244d4ca6
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 13:02:40 +0545

    add require user middleware

--- src/routes/v1/auth/index.ts
+++ src/routes/v1/auth/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authController from "./auth.controller";
+import requireLogin from "../../../middleware/requireLogin";
 
 const authRouter = Router();
 
 authRouter.post("/", authController.login);
-authRouter.post("/refresh", authController.refreshAccessToken);
+authRouter.post("/refresh", requireLogin, authController.refreshAccessToken);
 
 export default authRouter;
