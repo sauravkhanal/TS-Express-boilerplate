31202448d81386606aae9f857233b1ff559e26c5
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-23 15:21:31 +0545

    remove async

--- src/routes/v1/users/user.repository.ts
+++ src/routes/v1/users/user.repository.ts
@@ -2,52 +2,52 @@ import UserModel, { IUserDocument } from "../../../models/userModel";
 import { IUser } from "./types";
 
 const userRepository = {
-    async getAllUsers(): Promise<IUserDocument[] | null> {
+    getAllUsers(): Promise<IUserDocument[] | null> {
         return UserModel.find().select({ "password": false });
     },
 
-    async findUserByEmail(email: string): Promise<IUserDocument | null> {
+    findUserByEmail(email: string): Promise<IUserDocument | null> {
         return UserModel.findOne({ email }).select({ "password": false });
     },
 
-    async findUserById(_id: string): Promise<IUserDocument | null> {
+    findUserById(_id: string): Promise<IUserDocument | null> {
         return UserModel.findById({ _id }).select({ "password": false });
     },
 
-    async findUserByUsername(username: string): Promise<IUserDocument | null> {
+    findUserByUsername(username: string): Promise<IUserDocument | null> {
         return UserModel.findOne({ username }).select({ "password": false });
     },
 
-    async findUserByUsernameOrEmail(username: string, email: string): Promise<IUserDocument | null> {
+    findUserByUsernameOrEmail(username: string, email: string): Promise<IUserDocument | null> {
         return UserModel.findOne({ $or: [{ username }, { email }] }).select({ "password": false });
     },
 
-    async findUserByData(userData?: Partial<IUser>): Promise<IUserDocument | null> {
+    findUserByData(userData?: Partial<IUser>): Promise<IUserDocument | null> {
         return UserModel.findOne(userData).select({ "password": false });
     },
 
-    async createNewUser(userData: IUser): Promise<IUserDocument> {
+    createNewUser(userData: IUser): Promise<IUserDocument> {
         const newUser = new UserModel(userData);
         return newUser.save()
     },
 
-    async updateUserData(_id: string, newData?: IUser): Promise<IUserDocument | null> {
+    updateUserData(_id: string, newData?: IUser): Promise<IUserDocument | null> {
         return UserModel.findOneAndUpdate({ _id }, { ...newData }, { new: true }).select({ "password": false });
     },
 
-    async deleteUser(_id: string): Promise<IUserDocument | null> {
+    deleteUser(_id: string): Promise<IUserDocument | null> {
         return UserModel.findOneAndUpdate({ _id }, { deleted: true, deactivated: true }, { new: true }).select({ "password": false });
     },
 
-    async recoverUser(_id: string): Promise<IUserDocument | null> {
+    recoverUser(_id: string): Promise<IUserDocument | null> {
         return UserModel.findOneAndUpdate({ _id }, { deleted: false }, { new: true }).select({ "password": false });
     },
 
-    async deactivateUser(_id: string): Promise<IUserDocument | null> {
+    deactivateUser(_id: string): Promise<IUserDocument | null> {
         return UserModel.findOneAndUpdate({ _id }, { deactivated: true }, { new: true }).select({ "password": false });
     },
 
-    async reactivateUser(_id: string): Promise<IUserDocument | null> {
+    reactivateUser(_id: string): Promise<IUserDocument | null> {
         return UserModel.findOneAndUpdate({ _id }, { deactivated: false }, { new: true }).select({ "password": false });
     },
 
